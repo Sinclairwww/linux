88ffefcb0b4a8ca8408cb3c99b67abc8de157db7
Sinclair <sinclair_wuww@foxmail.com>
2023-01-22 23:40:33 +0800

    some mistakes

---
+++ src/1.22_3.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include <stdbool.h>
+#include <stdlib.h>
+struct TreeNode {
+    int              val;
+    struct TreeNode *left, *right;
+};
+
+bool isValidBST ( struct TreeNode* root ) {
+    if ( root == NULL || ( root->left == NULL && root->right == NULL ) ) {
+        return true;
+    } else if ( root->left == NULL && root->right != NULL ) {
+        if ( root->val >= root->right->val )
+            return false;
+    } else if ( root->right == NULL && root->left != NULL ) {
+        if ( root->val <= root->left->val )
+            return false;
+    } else if ( root->val <= root->left->val || root->val >= root->right->val ) {
+        return false;
+    }
+    return ( isValidBST ( root->left ) && isValidBST ( root->right ) );
+}
+
+
+// TODO
